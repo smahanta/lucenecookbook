9c35bc89a4cbdb372c5cdde5b9462d284e16c2f7
smahanta <subhendu_mahanta@yahoo.com>
2019-05-14 08:18:15 +0530

    add document to the index

--- lucenecookbook/src/main/java/com/packtpub/lucenecookbook/Recipe01.java
+++ lucenecookbook/src/main/java/com/packtpub/lucenecookbook/Recipe01.java
@@ -5,6 +5,9 @@ import java.nio.file.Paths;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
@@ -19,6 +22,11 @@ public class Recipe01 {
 		IndexWriterConfig config = new
 				IndexWriterConfig(analyzer);
 		IndexWriter indexWriter = new IndexWriter(directory, config); 
+		Document doc = new Document();
+		String text = "Lucene is an Information Retrieval library written in Java";
+		doc.add(new TextField("fieldName", text, Field.Store.YES));
+		indexWriter.addDocument(doc);
+		indexWriter.close();
 		
 	}
 
